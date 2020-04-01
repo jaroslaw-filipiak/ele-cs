406fc60a77f70cfad2fdd7c051b1f9f6c8474f5f
Jarek <info@j-filipiak.pl>
2020-04-01 16:22:19 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2 align="center">Eleph..e case study template</h2>
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'bootstrap';
+import './sass/main.scss';
+// import './../node_modules/slick-carousel/slick/slick.scss';
+// import './../node_modules/slicknav/scss/slicknav.scss';
+// import './../node_modules/slicknav/scss/core.scss';
+// import './../node_modules/slicknav/scss/theme.scss';
+// import './../node_modules/aos/src/sass/aos.scss';
+
+const $ = require('jquery');
+// const slick = require('slick-carousel');
+// const slicknav = require('slicknav/jquery.slicknav');
+// const waypoints = require('././../node_modules/waypoints/lib/jquery.waypoints');
+// const Parallax = require('parallax-js');
+// const AOS = require('aos');
+
+//AOS.init();
